94c91204b6f544e310e5c686d51e6b31e197d57c
SanyaShpot <shsasha152016@gmail.com>
2023-12-03 14:35:02 +0200

    fixed-the-code-in-lab5
    added docstrings
    deleted whitespaces
    replaced __init__ witch dataclass decorator for Cnady class
    split lines which was too long into 2
    renamed bad named variables and methods
    deleted unnecessary getters and docstrings
    added some blank lines for code easy-reading
    deleted unnecessary "else" in "ate" method
    fixed some other micro problems

--- laba 5.py
+++ laba 5.py
@@ -1,65 +1,74 @@
+"""modules for enum type and dataclass decorator"""
 from enum import Enum
+from dataclasses import dataclass
+
 
 class CandyType(Enum):
+    """Enum class for candy type"""
     BAR = 1
     BUTTON = 2
     POPCORN = 3
     GUM = 4
 
-class Candy():
-    def __init__(self, name, mass_in_grams, amount, price, type):
-        self.__name = name
-        self.__mass_in_grams = mass_in_grams
-        self.__amount = amount
-        self.__price = price
-        self.__type = type
-    
+
+@dataclass
+class Candy:
+    "Class for candies"
+    name: str
+    mass_in_grams: int
+    amount: int
+    price: int
+    typecandy: CandyType
+
     def __str__(self):
-        return f"{self.__name} - {self.__type} in the amount of {self.__amount}, weighing {self.__mass_in_grams} grams and costs {self.__price} UAH"
-    
+        return (
+            f"{self.name} - {self.typecandy} in the amount of {self.amount},"
+            f" weighing {self.mass_in_grams} grams and costs {self.price} UAH"
+        )
+
     def __repr__(self):
-        return f"Name: {self.__name}; Mass: {self.__mass_in_grams}; Amount: {self.__amount}; Price: {self.__price}; {self.__type}"
+        return (
+            f"Name: {self.name}; Mass: {self.mass_in_grams};"
+            f" Amount: {self.amount}; Price: {self.price}; {self.typecandy}"
+        )
 
     def ate(self):
-        if self.__mass_in_grams * self.__amount > 2000:
+        """Method for eating candy"""
+        if self.mass_in_grams * self.amount > 2000:
             return "You're on a diet!"
-        else:
-            return "What delicious candies!"
-    
-    def get_name(self):
-        return f'Name: {self.__name}'
-    
-    def get_amount(self):
-        return f'Amount: {self.__amount}'
-    
-    def get_price(self):
-        return f'Price: {self.__price}'
-    
-    def get_mass_in_grams(self):
-        return f'Mass: {self.__mass_in_grams} grams'
-    
-    def get_type(self):
-        return f'{self.__type}'
+        return "What delicious candies!"
 
-class Dinner():
+    def get_price(self):
+        """getter for candy price | used in findthemostexpensivecandies"""
+        return f'Price: {self.price}'
+
+
+class Dinner:
+    """Class for Dinner part of the task"""
     def __init__(self, day, time):
+        """init method for Diner class"""
         self.__day = day
         self.__time = time
         self.candies = []
-    
+
     def dinner_info(self):
+        """Returns information about dinner time and day of the week"""
         return f'It`s {self.__day} at {self.__time}:'
 
     def add_candy(self, __candy):
+        """Add candy in candies list for dinner"""
         self.candies.append(__candy)
 
-    def add(self, __new_candies):
+    def add_some_candies(self, __new_candies):
+        """Add more than 1 candy"""
         self.candies.extend(__new_candies)
-        
-    def findTheMostExpensiveCandies(self):
-        top_3 = sorted(self.candies, key = lambda x: x._Candy__price, reverse = True)
-        return top_3[:3]
-    
+
+    def findthemostexpensivecandies(self):
+        """Method for finding top 3 most expensive candies"""
+        top_3_most_expensive = sorted(self.candies, key=lambda x: x.get_price(), reverse=True)
+        return top_3_most_expensive[:3]
+
+
 if __name__ == '__main__':
     candy1 = Candy("Snickers", 50, 3, 51, CandyType.BAR)
     candy2 = Candy("CornPop", 120, 2, 30, CandyType.POPCORN)
@@ -68,14 +77,7 @@ if __name__ == '__main__':
     candy5 = Candy("Bounty", 50, 4, 48, CandyType.BAR)
 
     dinner1 = Dinner("Monday", "7:30 PM")
-    """
-    dinner1.add_candy(candy1)
-    dinner1.add_candy(candy2)
-    dinner1.add_candy(candy3)
-    dinner1.add_candy(candy4)
-    dinner1.add_candy(candy5)
-    """
-    dinner1.add([candy1, candy2, candy3, candy4, candy5])
+    dinner1.add_some_candies([candy1, candy2, candy3, candy4, candy5])
 
     print(Dinner.dinner_info(dinner1))
     for candy in dinner1.candies:
@@ -85,6 +87,6 @@ if __name__ == '__main__':
     print("-----"*20)
 
     print("Top 3 most expensive candies:")
-    top_3 = dinner1.findTheMostExpensiveCandies()
+    top_3 = dinner1.findthemostexpensivecandies()
     for i, candy in enumerate(top_3):
         print(f"{i+1}. {candy}")
